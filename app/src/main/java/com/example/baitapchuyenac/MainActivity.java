1c07e5c5fdcea39512fcb782eafb52fc9c86a96b
huyhoangy <huyhoangwf@gmail.com>
2025-10-05 19:36:28 +0700

    commit login lan 2

--- app/src/main/java/com/example/baitapchuyenac/MainActivity.java
+++ app/src/main/java/com/example/baitapchuyenac/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.baitapchuyenac;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btnLogin=findViewById(R.id.btn_Login);
+        btnLogin.setOnClickListener(view ->{
+           Intent myintent = new Intent(MainActivity.this,ProfileActivity.class);
+           startActivity(myintent);
         });
     }
 }
--- app/src/main/java/com/example/baitapchuyenac/ProfileActivity.java
+++ app/src/main/java/com/example/baitapchuyenac/ProfileActivity.java
@@ -1,7 +1,13 @@
 package com.example.baitapchuyenac;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    @Override
+    public  void onCreate(Bundle saveIntancseState){
+        super.onCreate(saveIntancseState);
+        setContentView(R.layout.activity_profile);
+    }
 }
