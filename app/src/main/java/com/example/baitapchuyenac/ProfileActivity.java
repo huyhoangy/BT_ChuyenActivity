efbd65db770bcaa1be73f31c7efb7d131c70ab97
huyhoangy <huyhoangwf@gmail.com>
2025-10-06 10:17:16 +0700

    commit profile lan 2

--- app/src/main/java/com/example/baitapchuyenac/ProfileActivity.java
+++ app/src/main/java/com/example/baitapchuyenac/ProfileActivity.java
@@ -1,13 +1,21 @@
 package com.example.baitapchuyenac;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
+    private ImageButton imgbtnsetting;
     @Override
     public  void onCreate(Bundle saveIntancseState){
         super.onCreate(saveIntancseState);
         setContentView(R.layout.activity_profile);
+        imgbtnsetting=findViewById(R.id.imgbtn_setting);
+        imgbtnsetting.setOnClickListener(view ->{
+            Intent myintent= new Intent(ProfileActivity.this,MainActivity.class);
+            startActivity(myintent);
+        });
     }
 }
